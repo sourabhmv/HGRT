93efb0397febdc5d5c4266a97115409140f374c9
Vishnu <vishnupothan2@gmail.com>
2021-06-03 13:39:17 +0530

    Small update

--- app/src/main/java/com/itsmesou/hgrtapp/MainActivity.java
+++ app/src/main/java/com/itsmesou/hgrtapp/MainActivity.java
@@ -67,7 +67,6 @@ public class MainActivity extends AppCompatActivity {
 
     int timer = 0;
 
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
