6a7abef2003ac432fd5ca206a724a96c39170cea
sourabhmv <sourabhmvmo@gmail.com>
2021-07-07 23:44:25 +0430

    before preview check

--- app/src/main/java/com/itsmesou/hgrtapp/Startfileencryption.java
+++ app/src/main/java/com/itsmesou/hgrtapp/Startfileencryption.java
@@ -5,10 +5,13 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.content.ContextCompat;
 
 import android.Manifest;
+import android.annotation.SuppressLint;
 import android.app.Activity;
+import android.content.ContentUris;
 import android.content.Context;
 import android.content.Intent;
 import android.content.SharedPreferences;
+import android.database.Cursor;
 import android.graphics.Bitmap;
 import android.graphics.drawable.BitmapDrawable;
 import android.graphics.drawable.Drawable;
@@ -17,6 +20,7 @@ import android.net.Uri;
 import android.os.Build;
 import android.os.Bundle;
 import android.os.Environment;
+import android.provider.DocumentsContract;
 import android.provider.MediaStore;
 import android.util.Log;
 import android.view.View;
@@ -38,6 +42,7 @@ import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.InputStream;
+import java.net.URISyntaxException;
 import java.nio.channels.FileChannel;
 import java.security.InvalidAlgorithmParameterException;
 import java.security.InvalidKeyException;
@@ -52,8 +57,8 @@ import Utils.MyEncrypter;
 public class Startfileencryption extends AppCompatActivity {
 
     private  ArrayList<Uri> imageUris= new ArrayList<Uri>();
-     private static final String FILE_NAME_ENC="aswinsmile_enc";
-    private static final String FILE_NAME_DEC="aswinsmile_dec.png";
+     private static  String FILE_NAME_ENC="image";
+    private static  String FILE_NAME_DEC="image";
      Button btnenc,btndec;
      File MyDir,MyDir2;
      ImageView imageView;
@@ -66,6 +71,7 @@ public class Startfileencryption extends AppCompatActivity {
     private static final int PICK_IMAGES_CODE=0;
     int position=0;
     Context context;
+    int count;
 
 
 
@@ -78,10 +84,12 @@ public class Startfileencryption extends AppCompatActivity {
         btndec=findViewById(R.id.btndecrypt);
         imageView=findViewById(R.id.imageview);
 
+
         // init path
 
         MyDir=new File(Environment.getExternalStorageDirectory().toString()+"/Encrypted_images");
         MyDir2=new File(Environment.getExternalStorageDirectory().toString()+"/Decrypted_images");
+
 
 
 
@@ -110,110 +118,48 @@ public class Startfileencryption extends AppCompatActivity {
                     }
                 }).check();
 
+
+
                  btndec.setOnClickListener(new View.OnClickListener() {
                      @Override
                      public void onClick(View v) {
 
-                         File outputFileDec = new File(MyDir,FILE_NAME_DEC);
-                         File encFile = new File(MyDir,FILE_NAME_ENC);
-                         try {
-                             MyEncrypter.decryptToFile(my_key,my_spec_key,new FileInputStream(encFile),
-                                     new FileOutputStream(outputFileDec));
+                         SharedPreferences sharedPreferences ;
+                         int flag2;
 
-                             // Seting image view
-                             imageView.setImageURI(Uri.fromFile(outputFileDec));
-                             // outputFileDec.delete();
+                         for(int i=0;i<count;i++) {
 
-                             // Move to decrypted folder
+                             sharedPreferences = getPreferences(MODE_PRIVATE);
+                             flag2 = sharedPreferences.getInt("key2", 1);
 
-                             copyOrMoveFile(outputFileDec,MyDir2,true);
-
-
-
-                             Toast.makeText(Startfileencryption.this, "Decrypted", Toast.LENGTH_SHORT).show();
+                             FILE_NAME_DEC="image"+flag2+".png";
+                             FILE_NAME_ENC="image"+flag2;
+                             flag2++;
+                             sharedPreferences = getPreferences(MODE_PRIVATE);
+                             SharedPreferences.Editor editor = sharedPreferences.edit();
+                             editor.putInt("key2", flag2);
+                             editor.commit();
 
 
-                         } catch (IOException e) {
-                             e.printStackTrace();
-                         } catch (NoSuchAlgorithmException e) {
-                             e.printStackTrace();
-                         } catch (InvalidKeyException e) {
-                             e.printStackTrace();
-                         } catch (InvalidAlgorithmParameterException e) {
-                             e.printStackTrace();
-                         } catch (NoSuchPaddingException e) {
-                             e.printStackTrace();
-                         }
-
-
-
-                     }
-                 });
-
-                 btnenc.setOnClickListener(new View.OnClickListener() {
-                     @Override
-                     public void onClick(View v) {
-
-                     // Create folder if it doesnt exists
-
-                         String folder_main = "Encrypted_images";
-                         String folder_main2 = "Decrypted_images";
-
-                         File f = new File(Environment.getExternalStorageDirectory(), folder_main);
-                         if (!f.exists()) {
-                             f.mkdirs();
-
-                         }
-
-
-                          f = new File(Environment.getExternalStorageDirectory(), folder_main2);
-                         if (!f.exists()) {
-                             f.mkdirs();
-
-                         }
-
-
-                         //convert to bitmap
-                        // Drawable drawable= ContextCompat.getDrawable(Startfileencryption.this,R.drawable.aswinsmile);
-                         //BitmapDrawable bitmapDrawable=(BitmapDrawable)drawable;
-
-                        // Bitmap bitmap=bitmapDrawable.getBitmap();
-                         if(imageUris.size()==0){
-                             Toast.makeText(context, "You have no selection", Toast.LENGTH_SHORT).show();
-                         }
-                         else {
-                             context=getApplicationContext();
-                             Bitmap bitmap = null;
+                             File outputFileDec = new File(MyDir, FILE_NAME_DEC);
+                             File encFile = new File(MyDir, FILE_NAME_ENC);
                              try {
-                                 bitmap = MediaStore.Images.Media.getBitmap(context.getContentResolver(), imageUris.get(0));
-                                 // path
-                                 String file_dj_path = Environment.getExternalStorageDirectory() +"/storage/emulated/0/Download/images (12).jpeg";
-                                 File fdelete = new File(file_dj_path);
-                                 if (fdelete.exists()) {
-                                     if (fdelete.delete()) {
-                                        Log.i("here file deleted",file_dj_path);
-                                         callBroadCast();
-                                     } else {
-                                         Log.i("here file not deleted", file_dj_path);
-                                     }
-                                 }
+                                 MyEncrypter.decryptToFile(my_key, my_spec_key, new FileInputStream(encFile),
+                                         new FileOutputStream(outputFileDec));
+
+                                 // Seting image view
+                                 imageView.setImageURI(Uri.fromFile(outputFileDec));
+                                 // outputFileDec.delete();
+
+                                 // Move to decrypted folder
+
+                                 copyOrMoveFile(outputFileDec, MyDir2, false);
 
 
-                             } catch (IOException e) {
-                                 Log.i("here try", "Error");
-                                 e.printStackTrace();
-                             }
+                                 Toast.makeText(Startfileencryption.this, "Decrypted", Toast.LENGTH_SHORT).show();
+                                 outputFileDec.delete();
 
-                             ByteArrayOutputStream stream = new ByteArrayOutputStream();
-                             bitmap.compress(Bitmap.CompressFormat.PNG, 100, stream);
-                             InputStream is = new ByteArrayInputStream(stream.toByteArray());
 
-                             //  create file
-                             File outputfileEnc = new File(MyDir, FILE_NAME_ENC);
-
-                             try {
-                                 MyEncrypter.encryptToFile(my_key, my_spec_key, is, new FileOutputStream(outputfileEnc));
-                                 Toast.makeText(Startfileencryption.this, "Encrypted", Toast.LENGTH_SHORT).show();
                              } catch (IOException e) {
                                  e.printStackTrace();
                              } catch (NoSuchAlgorithmException e) {
@@ -225,10 +171,13 @@ public class Startfileencryption extends AppCompatActivity {
                              } catch (NoSuchPaddingException e) {
                                  e.printStackTrace();
                              }
-
                          }
+
+
                      }
                  });
+
+
 
 
 
@@ -310,13 +259,137 @@ public class Startfileencryption extends AppCompatActivity {
     protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
         super.onActivityResult(requestCode, resultCode, data);
 
+
+
+        btnenc.setOnClickListener(new View.OnClickListener() {
+            private Object RandomStringUtils;
+
+            @Override
+            public void onClick(View v) {
+
+                // Create folder if it doesnt exists
+
+                String folder_main = "Encrypted_images";
+                String folder_main2 = "Decrypted_images";
+
+                File f = new File(Environment.getExternalStorageDirectory(), folder_main);
+                if (!f.exists()) {
+                    f.mkdirs();
+
+                }
+
+
+                f = new File(Environment.getExternalStorageDirectory(), folder_main2);
+                if (!f.exists()) {
+                    f.mkdirs();
+
+                }
+
+
+                //convert to bitmap
+                // Drawable drawable= ContextCompat.getDrawable(Startfileencryption.this,R.drawable.aswinsmile);
+                //BitmapDrawable bitmapDrawable=(BitmapDrawable)drawable;
+
+                // Bitmap bitmap=bitmapDrawable.getBitmap();
+                if(imageUris.size()==0){
+                    Toast.makeText(context, "You have no selection", Toast.LENGTH_SHORT).show();
+                }
+                else {
+                    context = getApplicationContext();
+                    Bitmap bitmap = null;
+                    int flag=1;
+                    SharedPreferences sharedPreferences;
+
+
+
+                    for (int i = 0; i < count; i++) {
+                    try {
+
+                         sharedPreferences = getPreferences(MODE_PRIVATE);
+                        flag = sharedPreferences.getInt("key1", 1);
+
+
+
+
+                            bitmap = MediaStore.Images.Media.getBitmap(context.getContentResolver(), imageUris.get(i));
+                            // path
+
+                            Uri uri = imageUris.get(i);
+                            Context context = Startfileencryption.this;
+                            String name = uri.getLastPathSegment();
+
+                            String file_dj_path = RealPathUtil.getRealPath(getApplicationContext(), uri);
+                            Log.i("Real path", file_dj_path);
+                            File fdelete = new File(file_dj_path);
+                            if (fdelete.exists()) {
+                                if (fdelete.delete()) {
+                                    Log.i("here file deleted", file_dj_path);
+                                    callBroadCast();
+                                } else {
+                                    Log.i("here file not deleted", file_dj_path);
+                                }
+                            } else {
+                                Log.i("path", file_dj_path);
+                            }
+
+
+                        } catch(IOException e){
+                            Log.i("here try", "Error");
+                            e.printStackTrace();
+                        }
+
+                        ByteArrayOutputStream stream = new ByteArrayOutputStream();
+                        bitmap.compress(Bitmap.CompressFormat.PNG, 100, stream);
+                        InputStream is = new ByteArrayInputStream(stream.toByteArray());
+
+                        //  create file
+                        FILE_NAME_DEC="image"+flag+".png";
+                        FILE_NAME_ENC="image"+flag;
+                        File outputfileEnc = new File(MyDir, FILE_NAME_ENC);
+                        flag++;
+                        sharedPreferences = getPreferences(MODE_PRIVATE);
+                        SharedPreferences.Editor editor = sharedPreferences.edit();
+                        editor.putInt("key1", flag);
+                        editor.commit();
+
+                        try {
+                            MyEncrypter.encryptToFile(my_key, my_spec_key, is, new FileOutputStream(outputfileEnc));
+                            Toast.makeText(Startfileencryption.this, "Encrypted", Toast.LENGTH_SHORT).show();
+                        } catch (IOException e) {
+                            e.printStackTrace();
+                        } catch (NoSuchAlgorithmException e) {
+                            e.printStackTrace();
+                        } catch (InvalidKeyException e) {
+                            e.printStackTrace();
+                        } catch (InvalidAlgorithmParameterException e) {
+                            e.printStackTrace();
+                        } catch (NoSuchPaddingException e) {
+                            e.printStackTrace();
+                        }
+
+
+
+                    }
+
+
+
+
+                }
+
+            }
+        });
+
+
+
+
+
         if(requestCode==PICK_IMAGES_CODE){
 
             if (resultCode== Activity.RESULT_OK){
 
                 if(data.getClipData()!=null){
                     //picked multiple images
-                    int count=data.getClipData().getItemCount(); //Number of images selected
+                     count=data.getClipData().getItemCount(); //Number of images selected
 
 
                         for (int i=0;i<count;i++){
@@ -335,6 +408,7 @@ public class Startfileencryption extends AppCompatActivity {
 
                     //pick single image
                     Uri imageUri=data.getData();
+                    count=1;
                     imageUris.add(imageUri);
                     Toast.makeText(this, "Success", Toast.LENGTH_SHORT).show();
                     position =0;
@@ -345,7 +419,90 @@ public class Startfileencryption extends AppCompatActivity {
         }
     }
 
+
+    //*********************************************************
+
+    @SuppressLint("NewApi")
+    public static String getFilePath(Context context, Uri uri) throws URISyntaxException {
+        String selection = null;
+        String[] selectionArgs = null;
+        // Uri is different in versions after KITKAT (Android 4.4), we need to
+        if (Build.VERSION.SDK_INT >= 19 && DocumentsContract.isDocumentUri(context.getApplicationContext(), uri)) {
+            if (isExternalStorageDocument(uri)) {
+                final String docId = DocumentsContract.getDocumentId(uri);
+                final String[] split = docId.split(":");
+                return Environment.getExternalStorageDirectory() + "/" + split[1];
+            } else if (isDownloadsDocument(uri)) {
+                final String id = DocumentsContract.getDocumentId(uri);
+                uri = ContentUris.withAppendedId(
+                        Uri.parse("content://downloads/public_downloads"), Long.valueOf(id));
+            } else if (isMediaDocument(uri)) {
+                final String docId = DocumentsContract.getDocumentId(uri);
+                final String[] split = docId.split(":");
+                final String type = split[0];
+                if ("image".equals(type)) {
+                    uri = MediaStore.Images.Media.EXTERNAL_CONTENT_URI;
+                } else if ("video".equals(type)) {
+                    uri = MediaStore.Video.Media.EXTERNAL_CONTENT_URI;
+                } else if ("audio".equals(type)) {
+                    uri = MediaStore.Audio.Media.EXTERNAL_CONTENT_URI;
+                }
+                selection = "_id=?";
+                selectionArgs = new String[]{
+                        split[1]
+                };
+            }
+        }
+        if ("content".equalsIgnoreCase(uri.getScheme())) {
+
+
+            if (isGooglePhotosUri(uri)) {
+                return uri.getLastPathSegment();
+            }
+
+            String[] projection = {
+                    MediaStore.Images.Media.DATA
+            };
+            Cursor cursor = null;
+            try {
+                cursor = context.getContentResolver()
+                        .query(uri, projection, selection, selectionArgs, null);
+                int column_index = cursor.getColumnIndexOrThrow(MediaStore.Images.Media.DATA);
+                if (cursor.moveToFirst()) {
+                    return cursor.getString(column_index);
+                }
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        } else if ("file".equalsIgnoreCase(uri.getScheme())) {
+            return uri.getPath();
+        }
+        return null;
+    }
+
+    public static boolean isExternalStorageDocument(Uri uri) {
+        return "com.android.externalstorage.documents".equals(uri.getAuthority());
+    }
+
+    public static boolean isDownloadsDocument(Uri uri) {
+        return "com.android.providers.downloads.documents".equals(uri.getAuthority());
+    }
+
+    public static boolean isMediaDocument(Uri uri) {
+        return "com.android.providers.media.documents".equals(uri.getAuthority());
+    }
+
+    public static boolean isGooglePhotosUri(Uri uri) {
+        return "com.google.android.apps.photos.content".equals(uri.getAuthority());
+    }
+
+    //********************************************************
+
+
     // function for image browsing end
+
+
+
 
 
     // Move to decrypted folder function
